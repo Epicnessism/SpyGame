ec30bf3b940af52dd57201e7ef7765fda4d0161f
Tony <tonywang0697@yahoo.com>
2017-07-22 04:47:36 -0400

    finished setting role values, created outline for the rest of the code

--- src/main/SpyGame.java
+++ src/main/SpyGame.java
@@ -11,23 +11,16 @@ public class SpyGame {
 		
 		int playerNum;
 		String blankConfirm;
-		ArrayList<String> players = new ArrayList<>();
-		//ArrayList playerOrder = new ArrayList<>();
-		ArrayList<String> words = new ArrayList<>();
-		
-		int spys;
-		int blanks;
-		
-		//get the number of players to generate playing field
+		//get the number of playerNames to generate playing field
 		System.out.println("Please indicate the number of players: ");
 		playerNum = sc.nextInt();
 		System.out.println("Blanks on?: (Y/N) ");
 		blankConfirm = sc.next();
-		
+
+		int spys;
+		int blanks;
 		//set spys
 		spys = Math.floorDiv(playerNum, 4);
-		//System.out.println(spys);
-		
 		//set blanks
 		if(blankConfirm.equalsIgnoreCase("Y")) {
 			blanks = Math.floorDiv(playerNum, 6);
@@ -37,40 +30,64 @@ public class SpyGame {
 			System.out.println("Blank not set. ");
 		}
 		
-		//get seed
-		//int seed = rand.nextInt();
-		int plebSeed = (int) (new Date().getTime()/1000000000);
-		int blankSeed = (int) (new Date().getTime()/800000000);
-		int spySeed = (int) (new Date().getTime()/1200000000);
+		//seeds for spy, blank, and pleb
+		int tempSpySeed = rand.nextInt(playerNum);
+		int tempBlankSeed = rand.nextInt(playerNum);
+		int tempPlebSeed = rand.nextInt(playerNum);
 		
-		//initialize pleb and spy words, initialize who is spy and pleb and blanks
-		foodSet foodset = new foodSet();
-		String plebWord = foodset.getWord(plebSeed);
-		String spyWord = foodset.getWord(spySeed);
+		//check to make sure tempSpySeed and tempPlebSeed are not the same!!!
 		
-		//check to make sure pleb and spy are not the same, otherwise keep trying
 		
-		//Players game1 = new Players(playerNum); //creates a playerset
+		//This is temporary, needs to move to setWords method later
+		ArrayList<String> playerWords = new ArrayList<String>(Arrays.asList(new String[playerNum]));
+		Collections.fill(playerWords, null);
+		
+		
+		//call setRoles to assign roles this also needs to be implemented later
+		ArrayList<Boolean> playerRoles = setRoles(playerNum, spys, blanks,tempSpySeed,tempBlankSeed);
+		String spyWord = "ds"; //placeholder call greenFood whatever thing here
+		String plebWord = "d"; //placeholder
+		
+		
+		ArrayList<String> playerNames = new ArrayList<>();
+		//fills out playerNames, also set words to playerWords depending on playerRoles booleans
 		System.out.println("Please enter names: ");
 		for(int i=0;i< playerNum ;i++) {
 			System.out.println("Name of player: ");
-			players.add(sc.nextLine());
-			//timeseed%playerNum = spy,spys--, loop this until spys = 0
+			playerNames.add(sc.nextLine());
+			if(playerRoles.get(i) == true) {
+				playerWords.set(i, spyWord);
+			}
+			else if(playerRoles.get(i) == false) {
+				playerWords.set(i, plebWord);
+			}
 		}
 		
+		
+		//start doing the encryption stuff here
+		
+		
+		//then need to make a kill off system to track spy or not
 		
 		
 	}
 	
-	
-	public ArrayList<Boolean> setRoles(int playerNum, int spys,int blankSeed, int spySeed) {
+	public static ArrayList<Boolean> setRoles(int playerNum, int spys,int blanks, int tempSpySeed, int tempBlankSeed) {
 		ArrayList<Boolean> playerRoles = new ArrayList<Boolean>(Arrays.asList(new Boolean[playerNum]));
 		Collections.fill(playerRoles, Boolean.FALSE);
 		
-		while(spys != 0) {
-			int tempSpySeed = spySeed % (playerNum+1);
-			if(playerRoles.get(tempSpySeed) != true) {
+		//run until all spys and blanks have been assigned
+		while(spys > 0 && blanks > 0) {
+			//int tempSpySeed = rand.nextInt(playerNum);
+			
+//			int tempSpySeed = temp % (playerNum+1);
+			if(playerRoles.get(tempSpySeed) != true && playerRoles.get(tempSpySeed) != null) {
 				playerRoles.set(tempSpySeed, true);
+				spys--;
+			}
+			if(playerRoles.get(tempBlankSeed) != true && playerRoles.get(tempBlankSeed) != null) {
+				playerRoles.set(tempBlankSeed, null);
+				blanks--;
 			}
 			
 		}
--- src/main/testRunner.java
+++ src/main/testRunner.java
@@ -1,18 +1,25 @@
 package main;
 
-import java.util.ArrayList;
+import java.util.*;
 
 //import java.lang.*;
 public class testRunner {
 	public static void main(String[] args) {
+		int playerNum = 6;
+		Random rand = new Random();
+		int temp = rand.nextInt(playerNum);
+		System.out.println(temp);
+//		int tempSpySeed = temp % (playerNum+1);
+//		System.out.println(tempSpySeed);
+		
 //		int pl = 9;
 //		System.out.println(pl % 5);
-		
-		ArrayList playerRoles = new ArrayList();
-		int playerNum = 6;
-		for(int i=1;i<=playerNum;i++) {
-			playerRoles.add(i);
-			System.out.println(playerRoles);
-		}
+//		
+//		ArrayList playerRoles = new ArrayList();
+//		int playerNum = 6;
+//		for(int i=1;i<=playerNum;i++) {
+//			playerRoles.add(i);
+//			System.out.println(playerRoles);
+//		}
 	}
 }
